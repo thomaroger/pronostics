ae2d1274e1ad5107c74fb10b49d1bbd95d5b9ee8
Thomas ROGER <thomaroger@gmail.com>
2011-09-25 01:13:41 +0200

    Refactorisation du MVC

--- commons/class/App.class.php
+++ commons/class/App.class.php
@@ -8,22 +8,32 @@ class App {
   
   public function __construct(){
     $this->autoload();
-    $this->generateHeader();
   }
   
   public function autoload(){
     $this->autoload = new Autoload();
   }
   
-  public function generateHeader(){
+  public function handleHeader(){
     if(file_exists(COMMONS_HEADER)){
       require_once(COMMONS_HEADER);
     }
   }
   
-  public function generateFooter(){
+  public function handleFooter(){
     if(file_exists(COMMONS_HEADER)){
       require_once(COMMONS_FOOTER);
+    }
+  }
+  
+  public function handleContent(){
+    $url = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+    $path = explode('/',$url['path']);
+    $count = count($path);
+    if($count == 3){
+      $this->autoload->autoloadAction($path[1],$path[2]);
+    }else {
+      $this->autoload->autoloadAction(APP_DEFAULT, !empty($path[1])?$path[1]:ACTION_DEFAULT); 
     }
   }
 }
--- web/index.php
+++ web/index.php
@@ -1,15 +1,8 @@
 <?php 
   require_once ('../commons/configuration/config.php');
   require_once ('../commons/scripts/app.php');
-
-  $url = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-  $path = explode('/',$url['path']);
-  if(count($path) == 3){
-    $app->autoload->autoloadAction($path[1],$path[2]);
-  }else{
-    $app->autoload->autoloadAction(APP_DEFAULT, ACTION_DEFAULT);
-  }
   
-
-  $app->generateFooter();
+  $app->handleHeader();
+  $app->handleContent();
+  $app->handleFooter();
 ?>
