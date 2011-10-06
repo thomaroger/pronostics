26152c0bafd2be191c23270dffcdb9d0425f1cbc
Thomas Roger <troger.solutec@tf1.fr>
2011-10-06 14:13:50 +0200

    Recuperation des pronostics

--- apps/frontend/controllers/games.controller.php
+++ apps/frontend/controllers/games.controller.php
@@ -8,12 +8,44 @@ class gamesController{
 	}
 	
 	public function action(){
-    	$userCourant = User::getUser();
+		$userCourant = User::getUser();
+		
+		$flag = $this->app->request->getParameter('pronosFlag');
+		if($flag){
+			$dayId = $this->app->request->getParameter('pronosDay');
+			$instDay = new Day();
+			$day = $instDay->getDayById($dayId);
+			$games = $day->getGames();
+			foreach ($games as $game){
+				$prognosis = $this->app->request->getParameter('pronos',$game->getGameId());
+				$prognosisWin = "";
+				if($prognosis['team1'] > $prognosis['team2']){
+					$prognosisWin = $game->getGameTeam1();
+				}else if($prognosis['team1'] < $prognosis['team2']){
+					$prognosisWin = $game->getGameTeam2();
+				}else{
+					$prognosisWin = 'nul';
+				}
+				/*$instPrognosis = new Prognosis();
+				$instPrognosis->insert();*/
+				var_dump($prognosis);
+				var_dump($prognosisWin);
+			}
+		}
+		
+    	
 		$dayId = $this->app->request->getParameter('day');
 		$instDay = new Day();
 		$day = $instDay->getDayById($dayId); 
-		var_dump($day);
-		var_dump($day->getGames());
+		$this->app->context->day = $day;
+		$this->app->context->games = $day->getGames();
+	}
+	
+	public function savePrognosis(){
+		var_dump($this);
+		$userCourant = User::getUser();
+		var_dump($userCourant->getUserId());
+		
 	}
 }
 ?>
--- apps/frontend/templates/games.template.php
+++ apps/frontend/templates/games.template.php
@@ -3,3 +3,25 @@
 	$ctx = $app->getContext();
 	$err = $app->getError();	
 ?>
+<div>
+	<form action="" name="prognosis" method="post">
+		<?php 
+			echo $ctx->day->getName()."<br />";
+			foreach ($ctx->games as $game){
+				echo $game->getGameTeam1(); 
+				$resultTeam1 = 0;
+				$resultTeam2 = 0;
+				?>
+				<input type="text" name="pronos[<?php echo $game->getGameId()?>][team1]" value="<?php echo $resultTeam1;?>"/>
+				-
+				<input type="text" name="pronos[<?php echo $game->getGameId()?>][team2]" value="<?php echo $resultTeam2;?>" />
+				<?php 
+				echo $game->getGameTeam2();
+				echo "<br />";
+			}
+		?>
+		<input type="hidden" name="pronosFlag" value="1" />
+		<input type="hidden" name="pronosDay" value="<?php echo $ctx->day->getDayId(); ?>" />
+		<input type="submit" name="" />
+	</form>
+</div>
